0ab7df177b10fd8607df3f368a07b97a6e5a0155
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-02-13 14:51:59 +0530

    Create medium.c

---
+++ medium.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+
+printf(" Enter a Three Digit Number:");
+scanf("%d" ,&a);
+b = (a / 10) % 10 ;
+prinf("Middle digit=%d\n" ,b);
+getch();
+return o;
+}
+
+
+
