369a9da63021dbf99766375fd071bba6b16f24a7
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-02-22 16:39:48 +0530

    Create hm.c

---
+++ hm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int min;
+    int h,m;
+    scanf("%d",&min);
+    h=min/60;
+    m=min%60;
+   printf( "the hour is %d\n the min is%d",h,m);
+   
+    
+    return 0;
+}
