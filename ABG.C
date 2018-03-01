ae6974078dc26148dbd70fb4b382260dde74879b
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-03-01 11:06:37 +0000

    Create ABG.C

---
+++ ABG.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+ printf("count the number of word/n");
+ 
+    scanf("%[^\n]s", s);
+
+    for (i = 0;s[i] != '\0';i++)
+
+    {
+
+   if (s[i] == ' ')
+
+  count++;    
+
+    }
+
+    printf("number of words in given string are: %d\n", count + 1);
+
+}
