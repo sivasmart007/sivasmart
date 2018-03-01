be1884f7283f7a350c828070365d1e6a4506bec6
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-03-01 10:47:00 +0000

    Create ccc.v

---
+++ ccc.v
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int count=0;
+char word[200];
+printf("Enter a word:");
+gets[words];
+{
+if((words==0)&&(space==0))
+word++;
+space++;
+}
+printf("\n Number of words: %d", words,space);
+return 0;
+}
