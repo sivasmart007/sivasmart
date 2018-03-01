d6f201733c64d99d82bdbf1c169a7de482a12300
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-03-01 11:16:39 +0000

    Create GH.C

---
+++ GH.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char a[100];
+	int n,i,count=0;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]==' ')
+	    {
+	        count++;
+	    }
+	}
+	printf("%d",count);
+	return 0;
+}
